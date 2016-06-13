ad4bf88a247edb7d1d3c16b049182724d423bd3b
imutlxy <1162981617@qq.com>
2016-06-13 15:36:11 +0800

    test

--- TestDesignPattern/app/src/main/java/com/lxy/builder/Man.java
+++
@@ -1,8 +0,0 @@
-package com.lxy.builder;
-
-/**
- * Created by lxy on 16/6/12.
- */
-
-public class Man extends Person{
-}
--- TestDesignPattern/app/src/main/java/com/lxy/singleinstance/SingleInstance.java
+++
@@ -1,8 +0,0 @@
-package com.lxy.singleinstance;
-
-/**
- * Created by lxy on 16/6/12.
- */
-
-public class SingleInstance {
-}
